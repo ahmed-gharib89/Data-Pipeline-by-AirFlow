da75d9d090e4a966046adc232078c1de2b1307f9
Ahmed Gharib <a.gharib89@yahoo.com>
2021-06-04 14:47:31 +0000

    Set schadual daily and start date from April 1st, 2021

--- dags/udac_example_dag.py
+++ dags/udac_example_dag.py
@@ -11,13 +11,13 @@ from helpers import SqlQueries
 
 default_args = {
     'owner': 'udacity',
-    'start_date': datetime(2019, 1, 12),
+    'start_date': datetime(2021, 4, 1),
 }
 
 dag = DAG('udac_example_dag',
           default_args=default_args,
           description='Load and transform data in Redshift with Airflow',
-          schedule_interval='0 * * * *'
+          schedule_interval='* 0 * * *'
         )
 
 start_operator = DummyOperator(task_id='Begin_execution',  dag=dag)
@@ -63,20 +63,22 @@ run_quality_checks = DataQualityOperator(
 )
 
 end_operator = DummyOperator(task_id='Stop_execution',  dag=dag)
-<<<<<<< HEAD
-=======
+
 
 start_operator >> stage_events_to_redshift
 start_operator >> stage_songs_to_redshift
+
 stage_events_to_redshift >> load_songplays_table
 stage_songs_to_redshift >> load_songplays_table
+
 load_songplays_table >> load_user_dimension_table
 load_songplays_table >> load_song_dimension_table
 load_songplays_table >> load_artist_dimension_table
 load_songplays_table >> load_time_dimension_table
+
 load_user_dimension_table >> run_quality_checks
 load_song_dimension_table >> run_quality_checks
 load_artist_dimension_table >> run_quality_checks
 load_time_dimension_table >> run_quality_checks
+
 run_quality_checks >> end_operator
->>>>>>> e36c14c27b6573e5b91f4a926f7164a4050b6492
